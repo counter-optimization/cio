ca387245ce8117226944b91b5da3480770ed8a19
Reshabh Sharma <reshabhsh@gmail.com>
2023-01-20 18:44:28 -0800

    Add eval for chacha20_poly1305 encrypt/decrypt

--- Makefile
+++ Makefile
@@ -25,6 +25,12 @@ ARGON2ID_PASSWD_LEN=100
 ARGON2ID_OUT_LEN=100
 ARGON2ID_NUM_ITER=1000
 
+EVAL_CHACHA20_POLY1305_ENCRYPT=eval_chacha20_poly1305_encrypt.o
+EVAL_CHACHA20_POLY1305_DECRYPT=eval_chacha20_poly1305_decrypt.o
+CHACHA20_POLY1305_MSG_LEN=100
+CHACHA20_POLY1305_AD_LEN=100
+CHACHA20_POLY1305_NUM_ITER=1000
+
 EVAL_START_TIME=$$(date +%F-%H:%M:%S-%Z)
 EVAL_DIR=$(EVAL_START_TIME)-eval
 
@@ -35,6 +41,10 @@ all: build_eval
 run_eval: build_eval
 	mkdir $(EVAL_DIR)
 	./eval_ed25519  $(ED25519_NUM_ITER) $(ED25519_MSG_LEN) > $(EVAL_DIR)/libsodium-ed25519.log 2>&1
+	./eval_chacha20_poly1305_encrypt $(CHACHA20_POLY1305_NUM_ITER) $(CHACHA20_POLY1305_MSG_LEN) $(CHACHA20_POLY1305_AD_LEN) \
+		> $(EVAL_DIR)/libsodium-chacha20-poly1305-encrypt.log 2>&1
+	./eval_chacha20_poly1305_decrypt $(CHACHA20_POLY1305_NUM_ITER) $(CHACHA20_POLY1305_MSG_LEN) $(CHACHA20_POLY1305_AD_LEN) \
+		> $(EVAL_DIR)/libsodium-chacha20-poly1305-decrypt.log 2>&1
 	./eval_aesni256gcm_encrypt  $(AESNI256GCM_NUM_ITER) $(AESNI256GCM_MSG_LEN) $(AESNI256GCM_AD_LEN) \
 		> $(EVAL_DIR)/libsodium-aesni256gcm-encrypt.log 2>&1
 	./eval_aesni256gcm_decrypt  $(AESNI256GCM_NUM_ITER) $(AESNI256GCM_MSG_LEN) $(AESNI256GCM_AD_LEN) \
@@ -43,7 +53,7 @@ run_eval: build_eval
 		> $(EVAL_DIR)/libsodium-argon2id.log 2>&1
 	echo done
 
-build_eval: eval_ed25519 eval_aesni256gcm_encrypt eval_aesni256gcm_decrypt eval_argon2id eval_chacha20poly1305
+build_eval: eval_ed25519 eval_aesni256gcm_encrypt eval_aesni256gcm_decrypt eval_argon2id eval_chacha20_poly1305_encrypt eval_chacha20_poly1305_decrypt
 
 eval_ed25519: eval_prereqs $(EVAL_ED25519)
 	$(CC) $(EVAL_ED25519) $(LIBSODIUM_AR) -o $@
@@ -54,11 +64,14 @@ eval_aesni256gcm_encrypt: eval_prereqs $(EVAL_AESNI256GCM_ENCRYPT)
 eval_aesni256gcm_decrypt: eval_prereqs $(EVAL_AESNI256GCM_DECRYPT)
 	$(CC) $(EVAL_AESNI256GCM_DECRYPT) $(LIBSODIUM_AR) -o $@
 
+eval_chacha20_poly1305_encrypt: eval_prereqs $(EVAL_CHACHA20_POLY1305_ENCRYPT)
+	$(CC) $(EVAL_CHACHA20_POLY1305_ENCRYPT) $(LIBSODIUM_AR) -o $@
+
+eval_chacha20_poly1305_decrypt: eval_prereqs $(EVAL_CHACHA20_POLY1305_DECRYPT)
+	$(CC) $(EVAL_CHACHA20_POLY1305_DECRYPT) $(LIBSODIUM_AR) -o $@
+
 eval_argon2id: eval_prereqs $(EVAL_ARGON2ID)
 	$(CC) $(EVAL_ARGON2ID) $(LIBSODIUM_AR) -o $@
-
-eval_chacha20poly1305: eval_prereqs
-	:
 
 eval_prereqs: | $(LIBSODIUM_BUILT)
 
@@ -81,6 +94,8 @@ clean_eval:
 	-rm eval_aesni256gcm_encrypt
 	-rm eval_aesni256gcm_decrypt
 	-rm eval_argon2id
+	-rm -f eval_chacha20_poly1305_encrypt
+	-rm -f eval_chacha20_poly1305_decrypt
 
 clean: clean_eval
 	-rm $(LIBSODIUM_BUILT)
---
+++ eval_chacha20_poly1305_decrypt.c
@@ -0,0 +1,131 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <assert.h> 
+#include <string.h> 
+
+#include "eval_util.h"
+
+#define EXPECTED_ARGC 3
+#define NUM_BENCH_ITER_ARG_IDX 1
+#define MSG_SIZE_ARG_IDX 2
+#define AD_SIZE_ARG_IDX 3
+
+extern size_t crypto_aead_chacha20poly1305_ietf_npubbytes(void);
+extern size_t crypto_aead_chacha20poly1305_ietf_keybytes(void);
+extern size_t crypto_aead_chacha20poly1305_ietf_abytes(void);
+extern int crypto_aead_chacha20poly1305_ietf_encrypt(
+    unsigned char *c, unsigned long long *clen_p, const unsigned char *m,
+    unsigned long long mlen, const unsigned char *ad, unsigned long long adlen,
+    const unsigned char *nsec, const unsigned char *npub,
+    const unsigned char *k);
+extern int crypto_aead_aes256gcm_decrypt(unsigned char *m,
+          unsigned long long *mlen_p, unsigned char *nsec,
+          const unsigned char *c, unsigned long long clen,
+          const unsigned char *ad, unsigned long long adlen,
+          const unsigned char *npub, const unsigned char *k);
+extern void crypto_aead_chacha20poly1305_ietf_keygen(unsigned char *);
+
+int
+main(int argc, char** argv)
+{
+  if (argc != EXPECTED_ARGC) {
+    printf("Usage: %s <num_benchmark_iterations> <size_of_message>\n", argv[0]);
+    exit(-1);
+  }
+
+  // seed non-crypto-secure PRNG, for generating message contents
+  srand(EVAL_UTIL_H_SEED);
+
+  // parse args
+  int num_iter = strtol(/*src=*/ argv[NUM_BENCH_ITER_ARG_IDX],
+			/*endptr=*/ (char**) NULL,
+			/*base=*/ 10);
+
+  unsigned long long msg_sz = strtol(argv[MSG_SIZE_ARG_IDX], (char**) NULL, 10);
+  unsigned long long additional_data_sz = strtol(argv[AD_SIZE_ARG_IDX], (char**) NULL, 10);
+
+  // allocate space for message
+  unsigned char* msg = malloc(msg_sz);
+  assert(msg && "Couldn't allocate msg bytes in eval_chacha20-poly1305-decrypt.c");
+
+  // allocate space for opened message
+  unsigned char* opened_msg = malloc(msg_sz);
+  assert(opened_msg && "Couldn't allocate opened_msg bytes in eval_chacha20-poly1305-decrypt.c");
+
+  // allocate space for additional data
+  unsigned char* additional_data = malloc(additional_data_sz);
+  assert(additional_data && "Couldn't allocate msg bytes in eval_aesni256gcm_encrypt.c");
+
+  // allocate space for signed message buffer
+  unsigned long long ciphertext_sz = msg_sz + crypto_aead_chacha20poly1305_ietf_abytes();
+  unsigned char* ciphertext = malloc(ciphertext_sz);
+  assert(msg && "Couldn't allocate signed msg bytes in eval_chacha20-poly1305-decrypt.c");
+    
+  // allocate space for secret and private keys
+  unsigned char* privk = malloc(crypto_aead_chacha20poly1305_ietf_keybytes());
+  assert(privk && "Couldn't allocate private key bytes in eval_chacha20-poly1305-decrypt.c");
+
+
+  // allocate space for decrypted message
+  unsigned char* decrypted_msg = malloc(msg_sz);
+  assert(decrypted_msg &&
+    "Couldn't allocate decrypted_msg bytes in eval_chacha20-poly1305-decrypt.c");
+
+  // allocate space for nonce
+  unsigned char* nonce = malloc(crypto_aead_chacha20poly1305_ietf_npubbytes());
+
+  // allocate space for timer reads
+  uint64_t* times = calloc(num_iter, sizeof(uint64_t));
+  assert(times &&
+	 "Couldn't allocate array for benchmark times in eval_chacha20-poly1305-decrypt.c");
+
+  volatile uint64_t start_time = 0;
+  volatile uint64_t end_time = 0;
+
+  // main loop
+  for (int cur_iter = 0; cur_iter < num_iter; ++cur_iter) {
+    // generate private key
+    crypto_aead_chacha20poly1305_ietf_keygen(privk);
+
+    // generate message
+    ciocc_eval_rand_fill_buf(msg, msg_sz);
+
+    // generate nonce
+    ciocc_eval_rand_fill_buf(nonce, sizeof nonce);
+
+    int encrypt_result = crypto_aead_chacha20poly1305_ietf_encrypt(
+        /*signed msg buf=*/ciphertext,
+        /*signed msg sz=*/&ciphertext_sz,
+        /*msg buf=*/msg,
+        /*msg sz=*/msg_sz,
+        /*additional data=*/additional_data,
+        /*additional data sz=*/additional_data_sz, NULL, nonce, privk);
+
+    assert(-1 != encrypt_result); // -1 on err, 0 on ok
+    
+    // start counting cycles
+    start_time = START_CYCLE_TIMER;
+
+	int decrypt_result = crypto_aead_aes256gcm_decrypt(decrypted_msg, &msg_sz,
+          NULL, ciphertext, ciphertext_sz, additional_data, additional_data_sz,
+          nonce, privk);
+
+    assert(-1 != decrypt_result);
+
+    // stop counting cycles
+    end_time = STOP_CYCLE_TIMER;
+
+    times[cur_iter] = end_time - start_time;
+
+    int cmp_result = strncmp((const char*)msg, (const char*)decrypted_msg, msg_sz);
+    assert(0 == cmp_result &&
+          "in eval-chacha20-poly1305-decrypt.c, error validating decrypted msg = msg");
+  }
+
+  // output the timer results
+  printf("eval_chacha20-poly1305-decrypt cycle counts for %d iterations\n", num_iter);
+  for (int ii = 0; ii < num_iter; ++ii) {
+    printf("%" PRIu64 "\n", times[ii]);
+  }
+}
