87ec6d043d9d66e2b8cbb5fe0eec7a260d62dd98
Alexandra Michael <aemichae@cs.washington.edu>
2023-01-31 00:41:40 +0000

    Added support for user arguments to eval.sh
    process_eval_data.py generates overhead bar charts for each crypto func and ablation; also generates cycle charts for baseline
    
    eval.sh calls process_eval_data.py
    
    updated gitignore

--- eval.sh
+++ eval.sh
@@ -3,21 +3,141 @@
 EVAL_START_TIME=$(TZ='America/Los_Angeles' date +%F-%H:%M:%S-%Z)
 TOP_EVAL_DIR=$EVAL_START_TIME-eval
 
+# defaults, set by user
+CC=`realpath ~/llvm-project/build/bin/clang`
+CHECKER_DIR=`realpath ./checker`
+LIBSODIUM_DIR=`realpath ./libsodium`
+NUM_MAKE_JOB_SLOTS=8
+
+EXTRA_MAKEFILE_FLAGS=""
+
+BASELINE_DIR="baseline"
 CS_DIR="cs"
 SS_DIR="ss"
 SS_CS_DIR="ss-cs"
 CS_SS_DIR="cs-ss"
 
+function usage
+{
+    echo "Usage: ./eval.sh [ -h | --help (displays this message) ]
+			   [ -c | --cc <path to c compiler> ]
+			   [ -p | --checker-dir <path to root checker dir> ]
+			   [ -t | --crypto-dir <path to the crypto lib project that has the root makefile> ]
+			   [ -m | --makefile-flags \"<~double quoted string~ of extra flags for the Makefile>\" ]
+			   [ -j <num make job slots> ]"
+    exit 2
+}
+
+PARSED_ARGS=$(getopt -o "hc:p:t:m:j:" -l "help,cc:,checker-dir:,crypto-dir:,makefile-flags:" -n eval.sh -- "$@")
+
+if [[ $? -ne 0 ]]; then
+       echo "Error parsing args"
+       usage
+fi
+
+eval set -- "$PARSED_ARGS"
+unset PARSED_ARGS
+
+while true; do
+    case "$1" in
+	'-h' | '--help')
+	    usage
+	    ;;
+	'-j')
+	    NUM_MAKE_JOB_SLOTS=$2
+	    shift 2
+	    continue
+	    ;;
+	'-c' | '--cc')
+	    echo CC opt
+	    CC=$2
+	    shift 2
+	    continue
+	    ;;
+	'-p' | '--checker-dir')
+	    CHECKER_DIR=$2
+	    shift 2
+	    continue
+	    ;;
+	'-m' | '--makefile-flags')
+	    EXTRA_MAKEFILE_FLAGS=$2
+	    shift 2
+	    continue
+	    ;;
+	'-t' | '--crypto-dir')
+	    TARGET_DIR=$2
+	    shift 2
+	    continue
+	    ;;
+	'--')
+	    shift
+	    break
+	    ;;
+	*)
+	    echo "Unknown option $1"
+	    usage
+	    ;;
+    esac
+    shift
+done
+
 mkdir $TOP_EVAL_DIR
 
+# baseline
+make MITIGATIONS="" EVAL_DIR="$TOP_EVAL_DIR/$BASELINE_DIR" \
+    CC=$CC CHECKER_DIR=$CHECKER_DIR LIBSODIUM_DIR=$LIBSODIUM_DIR \
+    NUM_MAKE_JOB_SLOTS=8 EXTRA_MAKEFILE_FLAGS=$EXTRA_MAKEFILE_FLAGS \
+    run_eval
+
+if [[ $? -ne 0 ]]; then
+       echo "Error running baseline"
+       exit -1
+fi
+
 # cs only
-make MITIGATIONS="--cs" EVAL_DIR="$TOP_EVAL_DIR/$CS_DIR" run_eval
+make MITIGATIONS="--cs" EVAL_DIR="$TOP_EVAL_DIR/$CS_DIR" \
+    CC=$CC CHECKER_DIR=$CHECKER_DIR LIBSODIUM_DIR=$LIBSODIUM_DIR \
+    NUM_MAKE_JOB_SLOTS=8 EXTRA_MAKEFILE_FLAGS=$EXTRA_MAKEFILE_FLAGS \
+    run_eval
+
+if [[ $? -ne 0 ]]; then
+       echo "Error running cs mitigations"
+       exit -1
+fi
 
 # ss only
-make MITIGATIONS="--ss" EVAL_DIR="$TOP_EVAL_DIR/$SS_DIR" run_eval
+make MITIGATIONS="--ss" EVAL_DIR="$TOP_EVAL_DIR/$SS_DIR" \
+    CC=$CC CHECKER_DIR=$CHECKER_DIR LIBSODIUM_DIR=$LIBSODIUM_DIR \
+    NUM_MAKE_JOB_SLOTS=8 EXTRA_MAKEFILE_FLAGS=$EXTRA_MAKEFILE_FLAGS \
+    run_eval
+
+if [[ $? -ne 0 ]]; then
+       echo "Error running ss mitigations"
+       exit -1
+fi
 
 # cs then ss
-make MITIGATIONS="--cs --ss" EVAL_DIR="$TOP_EVAL_DIR/$CS_SS_DIR" run_eval
+make MITIGATIONS="--cs --ss" EVAL_DIR="$TOP_EVAL_DIR/$CS_SS_DIR" \
+    CC=$CC CHECKER_DIR=$CHECKER_DIR LIBSODIUM_DIR=$LIBSODIUM_DIR \
+    NUM_MAKE_JOB_SLOTS=8 EXTRA_MAKEFILE_FLAGS=$EXTRA_MAKEFILE_FLAGS \
+    run_eval
+
+if [[ $? -ne 0 ]]; then
+       echo "Error running cs-ss mitigations"
+       exit -1
+fi
 
 # ss then cs
-make MITIGATIONS="--ss --cs" EVAL_DIR="$TOP_EVAL_DIR/$SS_CS_DIR" run_eval
+make MITIGATIONS="--ss --cs" EVAL_DIR="$TOP_EVAL_DIR/$SS_CS_DIR" \
+    CC=$CC CHECKER_DIR=$CHECKER_DIR LIBSODIUM_DIR=$LIBSODIUM_DIR \
+    NUM_MAKE_JOB_SLOTS=8 EXTRA_MAKEFILE_FLAGS=$EXTRA_MAKEFILE_FLAGS \
+    run_eval
+
+if [[ $? -ne 0 ]]; then
+       echo "Error running ss-cs mitigations"
+       exit -1
+fi
+
+# generate plots
+python3 process_eval_data.py $EVAL_DIR $BASELINE_DIR \
+    $CS_DIR $SS_DIR $CS_SS_DIR $SS_CS_DIR
--- process_eval_data.py
+++ process_eval_data.py
@@ -1,15 +1,18 @@
 import argparse
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
-EVAL_CASES = [
-    'libsodium-ed25519',
-    'libsodium-aesni256gcm-decrypt',
-    'libsodium-aesni256gcm-encrypt',
-    'libsodium-argon2id',
-    'libsodium-chacha20-poly1305-decrypt',
-    'libsodium-chacha20-poly1305-encrypt'
-]
+CRYPTO_FNS = dict({
+    'libsodium':
+    [   'ed25519'
+    ,   'aesni256gcm-decrypt'
+    ,   'aesni256gcm-encrypt'
+    ,   'argon2id'
+    ,   'chacha20-poly1305-decrypt'
+    ,   'chacha20-poly1305-encrypt'
+    ]
+})
 
 
 def parse_lines(filepath):
@@ -27,39 +30,115 @@ def parse_lines(filepath):
     return data
 
 
+def gen_cycle_curves(eval_dir, subdir):
+    ''' 
+    Generate cycle line charts for each crypto func test case in a subdirectory.
+    Useful for gauging number of warmup iterations.
+    '''
+    for lib in CRYPTO_FNS:
+        for fn in CRYPTO_FNS[lib]:
+            # Get data
+            logfile = os.path.join(eval_dir, subdir, f'{lib}-{fn}.log')
+            raw_data = parse_lines(logfile)
+            title = f'{subdir}: {raw_data[0]}'
+            cycles_data = raw_data[1:]
+
+            min_cycles = min(cycles_data)
+            min_idx = cycles_data.index(min_cycles)
+            print(f'min of {lib}-{fn} is {min_cycles} at {min_idx}')
+
+            # # Calculate reasonable bounds for y-axis
+            # quartiles = np.quantile(cycles_data, [0.25, 0.75])
+            # iqr = quartiles[1] - quartiles[0]
+            # upper_bound = quartiles[1] + iqr * 4
+            # lower_bound = quartiles[0] - iqr * 2
+
+            # Plot
+            fig, ax = plt.subplots()
+            ax.clear()
+            ax.plot(cycles_data)
+            # ax.set_ylim(bottom=lower_bound, top=upper_bound)
+            ax.set_title(title)
+            ax.set_ylabel('Cycles')
+            ax.set_xlabel('Iteration')
+            fig.savefig(os.path.join(eval_dir, subdir, f'{lib}-{fn}-cycles.png'))
+
+
+def get_avg_cycles(eval_dir, subdir, test_case):
+    ''' Get average cycle counts for a single test case. '''
+    # TODO: geomean?
+    logfile = os.path.join(eval_dir, subdir, f'{test_case}.log')
+    raw_data = parse_lines(logfile)
+    cycles_data = raw_data[1:]
+    return sum(cycles_data) / len(cycles_data)
+
+
+def get_cycle_overheads(eval_dir, baseline, ablations):
+    ''' Calculate runtime cycle overheads for all '''
+    # Calculate baseline average cycles for each crypto func
+    baseline_avgs = dict()
+    for lib in CRYPTO_FNS:
+        lib_avgs = dict()
+        for fn in CRYPTO_FNS[lib]:
+            test_case = f'{lib}-{fn}'
+            lib_avgs[fn] = get_avg_cycles(eval_dir, baseline, test_case)
+        baseline_avgs[lib] = lib_avgs
+
+    # Calculate overheads for each ablation and crypto func
+    overheads = dict()
+    for lib in CRYPTO_FNS:
+        lib_ohs = dict()
+        for fn in CRYPTO_FNS[lib]:
+            fn_ohs = dict()
+            for abl in ablations:
+                test_case = f'{lib}-{fn}'
+                avg_cycles = get_avg_cycles(eval_dir, abl, test_case)
+                fn_ohs[abl] = avg_cycles / baseline_avgs[lib][fn]
+            lib_ohs[fn] = fn_ohs
+        overheads[lib] = lib_ohs
+
+    return overheads
+
+
+def gen_overhead_plot(eval_dir, baseline, ablations, out_dir):
+    ''' Create plot of runtime overhead for each ablation vs baseline.'''
+    overheads = get_cycle_overheads(eval_dir, baseline, ablations)
+    print(overheads)
+    
+    # Plot overheads
+    fig, ax = plt.subplots()
+    lib_ohs = overheads['libsodium']
+
+    for fn in lib_ohs:
+        fn_ohs = lib_ohs[fn]
+        ax.bar(fn_ohs.keys(), fn_ohs.values())
+        print(fn_ohs)
+        fig.savefig(os.path.join(out_dir, f'{fn}-plot.png'))
+        ax.clear()
+
+
 def main():
-    # Parse argument for eval directory
+    # Parse user arguments
     parser = argparse.ArgumentParser()
+
     parser.add_argument('eval_dir', help='directory containing the raw eval data')
+    parser.add_argument('baseline_dir',
+        help='subdirectory of eval_dir containing baseline data for comparison')
+    parser.add_argument(
+        'ablations',
+        nargs='+',
+        help='list of mitigation versions to compare. Example: `cs ss` would generate '
+             'a comparison between cs and ss only. Each ablation MUST have a '
+             'subdirectory with the same name in `eval_dir`.'
+    )
+    parser.add_argument('-o', '--out', help="output directory. Defaults to `eval_dir`")
+
     args = parser.parse_args()
 
-    eval_dir = args.eval_dir
-    eval_cases = EVAL_CASES
-
-    eval_paths = dict(map(
-        lambda case: (case, os.path.join(eval_dir, case + '.log')),
-        eval_cases
-    ))
-
-    eval_data = dict(map(lambda case: (case, parse_lines(eval_paths[case])), eval_cases))
-    eval_avgs = dict(map(
-        lambda case: (case, sum(eval_data[case][1:])/len(eval_data[case][1:])),
-        eval_cases
-    ))
-
-    # Output average cycles for each eval case
-    outfile = open(os.path.join(eval_dir, 'avg-cycles.txt'), "w")
-    for case in eval_cases:
-        header = eval_data[case][0]
-        outfile.writelines("Average " + header + "\n")
-        outfile.write(str(eval_avgs[case]) + "\n")
-    outfile.close()
-    
-    # Simple bar chart of average cycles
-    # warning: ugly/unreadable, partly because argon2id's cycle counts are so high
-    fig, ax = plt.subplots()
-    ax.bar(eval_avgs.keys(), eval_avgs.values())
-    fig.savefig(os.path.join(eval_dir, 'plot.png'))
+    # Generate cycle overhead plot
+    out_dir = args.out if args.out is not None else args.eval_dir
+    gen_overhead_plot(args.eval_dir, args.baseline_dir, args.ablations, out_dir)
+    gen_cycle_curves(args.eval_dir, args.baseline_dir)
 
 
 if __name__ == "__main__":
