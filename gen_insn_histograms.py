d3205491d8cb2a616700126622f1d17b8b9e3c4b
Alexandra Michael <aemichae@cs.washington.edu>
2023-08-01 20:43:59 +0000

    [Insn histograms] Modularize functions for getting and diffing histograms

--- gen_insn_histograms.py
+++ gen_insn_histograms.py
@@ -1,5 +1,65 @@
 import os
 
+def get_histogram_for_function(file_contents : str, func_name : str):
+    ''' Get instruction histogram for a function. The resulting histogram
+        is a dict with instruction names as keys and counts of the
+        corresponding instructions as values. This function makes certain
+        assumptions about the format of the provided file contents, consistent
+        with disassembled binaries produced by `objdump` with the flags
+        `-d --no-addresses --no-show-raw-insn`. The flag `-M suffix` is also
+        recommended, to disambiguate between instructions that perform the
+        same operation at different bitwidths.
+        '''
+    print(f"Calculating instruction histogram for function {func_name}...")
+
+    start = file_contents.find(f'{func_name}>:\n')
+    if start < 0:
+        print(f"ERROR: Could not find function {func_name} in file")
+        return
+    
+    end = file_contents.find('\n\n', start)
+    lines = file_contents[start : end].split('\n\t')[1:]
+    histo = dict[str,int]()
+
+    for line in lines:
+        insn = line.split()[0]
+        if insn in histo.keys():
+            histo[insn] += 1
+        else:
+            histo[insn] = 1
+
+    return histo
+
+
+def diff_histograms(original : dict[str,int], transformed : dict[str,int]):
+    ''' Calculate the difference histogram between an original and transformed function.
+        The difference histogram represents the modifications to instruction counts present
+        in the transformed function versus the original, i.e.,
+        transformed histogram - original histogram. Instructions whose counts are the same
+        in both (i.e., a diff of 0) are omitted.
+        
+        In general, we expect to see a higher count of any given instruction in the
+        transformed function. Negative diffs, indicating that an instruction was removed
+        from the transformed function compared to the original, should only be present
+        inasmuch as those instructions were replaced by equivalent sequences. In other
+        words, the number of instructions removed from the transformed function should
+        be no greater than the number of instructions in the original function to which
+        some transformation was applied.
+    '''
+    diff = dict[str,int]()
+    for insn in transformed.keys():
+        if insn not in original.keys():
+            diff[insn] = transformed[insn]
+        elif transformed[insn] - original[insn] != 0:
+            diff[insn] = transformed[insn] - original[insn]
+
+    for insn in original.keys():
+        if insn not in diff.keys():
+            diff[insn] = -1 * original[insn]
+
+    return diff
+
+
 def gen_reference_histograms():
     ref_binary_filepath = os.path.join(
         '.',
@@ -65,18 +125,7 @@ def gen_reference_histograms():
         print(f"Transformed histogram: {trns_histo}")
         
         # save difference between transformed and original instruction counts
-        histograms[name] = dict()
-        for insn in trns_histo.keys():
-            if insn not in orig_histo.keys():
-                histograms[name][insn] = trns_histo[insn]
-            elif trns_histo[insn] - orig_histo[insn] != 0:
-                histograms[name][insn] = trns_histo[insn] - orig_histo[insn]
-        
-        # check for instructions that appear in original but not transformed function
-        # (should be at most one)
-        for insn in orig_histo.keys():
-            if insn not in trns_histo.keys():
-                histograms[name][insn] = -1 * orig_histo[insn]
+        histograms[name] = diff_histograms(orig_histo, trns_histo)
         
         print(f"Generated histogram for {name}:")
         print(histograms[name])
